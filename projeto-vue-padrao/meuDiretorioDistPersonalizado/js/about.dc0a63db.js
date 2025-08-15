b8ec7d1ddd3c1199b88cb21cf0d3301d1081ae65
Leonardo Rosario <leorosario9605@gmail.com>
2025-08-15 14:33:36 -0300

    AULA 99

---
+++ projeto-vue-padrao/meuDiretorioDistPersonalizado/js/about.dc0a63db.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{d7a9:function(n,t,a){"use strict";a.r(t);var e=a("7a23");const c={class:"about"};function o(n,t){return Object(e["q"])(),Object(e["d"])("div",c,t[0]||(t[0]=[Object(e["e"])("h1",null,"This is an about page",-1)]))}var s=a("6b0d"),u=a.n(s);const b={},d=u()(b,[["render",o]]);t["default"]=d}}]);
+//# sourceMappingURL=about.dc0a63db.js.map
