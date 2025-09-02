1481c87aafa996debd00efca2857b761ca07acd9
Leonardo Rosario <leorosario9605@gmail.com>
2025-09-02 15:42:36 -0300

    Aula 132

--- vagas-front/src/components/comuns/Vaga.vue
+++ vagas-front/src/components/comuns/Vaga.vue
@@ -1,17 +1,51 @@
 <template>
     <div class="card">
-        <div class="card-header bg-dark text-white">Título da vaga</div>
+        <div class="card-header bg-dark text-white">{{ titulo }}</div>
         <div class="card-body">
-        <p>Descrição truncada da vaga...</p>
+        <p>{{ descricao }}</p>
         </div>
         <div class="card-footer">
-        <small class="text-muted">Salário: R$ 6.000,00 | Modalidade: Home Office | Tipo: CLT | Publicação: 10/11/2021</small>
+        <small class="text-muted">Salário: {{ salario }} | Modalidade: {{ modalidade }} | Tipo: {{ tipo }} | Publicação: {{ publicacao }}</small>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Vaga"
+    name: "Vaga",
+    // props: ['tituloVagaTeste', 'descricaoVaga', 'salario', 'modalidade', 'tipo', 'publicacao'],
+    props: {
+        titulo: {
+            type: String,
+            required:  true,
+            validator(p) {
+                //console.log('Prop: ', p, p.length);
+                if(p.length < 6) return false; // se estiver inválido
+                return true; // se estiver válido
+            }
+        },
+        descricao: {
+            type: String,            
+            default() {
+                return '*'.repeat(20);
+            }
+        },
+        salario: {
+            type: [Number, String],
+            required: true
+        },
+        modalidade: {
+            type: String,
+            required: true
+        },
+        tipo: {
+            type: String,
+            required: true
+        },
+        publicacao: {
+            type: String,
+            required: true
+        },
+    }
 }
 </script>
--- vagas-front/src/components/views/PublicarVaga.vue
+++ vagas-front/src/components/views/PublicarVaga.vue
@@ -1,12 +1,86 @@
 <template>
-  <div>
-    <h1>Publicar Vaga</h1>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col">
+        <h4>Apresente a sua vaga para milhares de profissionais e de graça.</h4>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <label class="form-label">Título da Vaga</label>
+        <input type="text" class="form-control" v-model="titulo">
+        <div class="form-text">Por exemplo: Programador Javascript e VueJS.</div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <label class="form-label">Descrição</label>
+        <textarea type="text" class="form-control" v-model="descricao"></textarea>
+        <div class="form-text">Informe os detalhes da vaga.</div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <label class="form-label">Salário</label>
+        <input type="number" class="form-control" v-model="salario">
+        <div class="form-text">Informe o salário.</div>
+      </div>
+
+      <div class="col">
+        <label class="form-label">Modalidade</label>
+        <select class="form-select" v-model="modalidade">
+            <option value="" disabled>--Selecione</option>
+            <option value="1">Home Office</option>
+            <option value="2">Presencial</option>
+        </select>
+        <div class="form-text">Informe a modalidade.</div>
+      </div>
+
+      <div class="col">
+        <label class="form-label">Tipo</label>
+        <select class="form-select" v-model="tipo">
+            <option value="" disabled>--Selecione</option>
+            <option value="1">CLT</option>
+            <option value="2">PJ</option>
+        </select>
+        <div class="form-text">Informe o tipo de contratação.</div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <button class="btn btn-primary" type="submit" @click="salvarVaga()">Cadastrar</button>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default{
-  name: 'PublicarVaga'
+  name: 'PublicarVaga',
+  data: () => ({
+    titulo: '',
+    descricao: '',
+    salario: '',
+    modalidade: '',
+    tipo: ''
+  }),
+  methods: {
+    salvarVaga(){
+      let vaga = {
+        titulo: this.titulo,
+        descricao: this.descricao,
+        salario: this.salario,
+        modalidade: this.modalidade,
+        tipo: this.tipo
+      }
+      localStorage.setItem('vagas', JSON.stringify(vaga));
+    }
+  }
 }
 </script>
 
