a730be68ffdfa01753ef86594200228a9e135251
Leonardo Barbosa Silva do Rosário <leorosario9605@gmail.com>
2025-09-30 14:38:20 -0300

    Projeto mixins finalizado

---
+++ mixins/src/components/ListaFilmes.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h3>{{ titulo }}</h3>
+        <input type="text" placeholder="Nome do filme" v-model="item">
+        <button type="button" @click="adicionarItem">Adicionar</button>
+        <br>
+        <ul>
+            <li v-for="(item, index) in itens" :key="index">{{ item }}</li>
+        </ul>
+        <button type="button" @click="removerItem">Remover</button>
+    </div>
+    
+</template>
+
+<script>
+import ListaMixin from '@/mixins/ListaMixin';
+import RemoverItemListaMixin from '@/mixins/RemoverItemListaMixin';
+export default{
+    name: 'ListaFilmes',
+    mixins: [ListaMixin, RemoverItemListaMixin],
+    data: () => ({
+        titulo: 'Lista Filmes'
+    }),
+    created(){
+        console.log('ListaFilmes criado');
+    }
+}
+</script>
