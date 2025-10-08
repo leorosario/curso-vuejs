17e9181439a1805a58113731ad73b998fafc2916
Leonardo Rosario <leorosario9605@gmail.com>
2025-10-08 16:19:17 -0300

    AULA 243

--- empresa360/src/router.js
+++ empresa360/src/router.js
@@ -5,11 +5,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Contratos from '@/components/vendas/Contratos.vue';
 import Dashboard from '@/components/dashboard/Dashboard.vue';
 import Home from '@/views/Home.vue';
+import Lead from '@/components/vendas/Lead.vue';
 import Leads from '@/components/vendas/Leads.vue';
 import Login from '@/views/Login.vue';
 import Servicos from '@/components/servicos/Servicos.vue';
 import Site from '@/views/Site.vue';
 import Vendas from '@/components/vendas/Vendas.vue';
+import VendasPadrao from '@/components/vendas/VendasPadrao.vue';
 
 
 const routes = [
@@ -23,11 +25,13 @@ const routes = [
         children: [
             { path: 'vendas', component: Vendas, children: 
                 [
-                    { path: 'leads', component: Leads}, //localhost:8080/home/vendas/leads
-                    { path: 'contratos', component: Contratos} //localhost:8080/home/vendas/contratos
+                    { path: 'leads', component: Leads, name: 'leads'}, //localhost:8080/home/vendas/leads
+                    { path: 'leads/:id', component: Lead, name: 'lead'}, //localhost:8080/home/vendas/leads/5
+                    { path: 'contratos', component: Contratos, name: 'contratos'}, //localhost:8080/home/vendas/contratos
+                    { path: '', component: VendasPadrao} //localhost:8080/home/vendas/
                 ]
             }, //localhost:8080/home/vendas
-            { path: 'servicos', component: Servicos}, //localhost:8080/home/servicos
+            { path: 'servicos', component: Servicos, name: 'servicos'}, //localhost:8080/home/servicos
             { path: 'dashboard', component: Dashboard} //localhost:8080/home/dashboard
         ]
     },
