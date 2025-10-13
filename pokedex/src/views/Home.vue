b970b92bd5385cacb129c7187efc708f6e82826c
Leonardo Rosario <leorosario9605@gmail.com>
2025-10-13 15:17:35 -0300

    Iniciado projeto pokedex

---
+++ pokedex/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="row mt-2">
+      <div class="col mb-2">palco</div>
+      <div class="col mb-2">pokédex</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home' 
+}
+</script>
