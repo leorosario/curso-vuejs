05d4ef41b710eff9a322cf2f11d12f0570768da2
Leonardo Rosario <leorosario9605@gmail.com>
2025-10-16 16:10:51 -0300

    AULA 343

---
+++ emergenciasmedicas/src/components/ConfiguracaoEquipe.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col">
+        <h5 :class="corTitulo"><i class="bi-boxes me-2"></i>{{ tituloCustomizadoLocal }}</h5>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-8">
+        <p>Enfermeiro: {{ enfermeiro }}</p>
+        <p>Socorrista: {{ socorrista }}</p>
+        <p>Médico: {{ medico }}</p>
+        <p>Carro: {{ carro }}</p>
+        <p>Telefone: {{ telefone }}</p>
+        <p>Kit de reanimação: {{ kitDeReanimacao }}</p>
+      </div>
+      <div class="col-4 text-center">
+        <div class="row">
+          <div class="col">
+            <img class="img-fluid" :src="require('@/assets/ambulancias/indefinida.png')">
+          </div>
+        </div>
+        <div class="row mt-3">
+          <div class="col">
+            <button type="button" class="btn btn-primary">Montar equipe</button>
+          </div>
+        </div>              
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'ConfiguracaoEquipe',
+  data: () => ({
+    titulo: 'Configuração da equipe'
+  }),
+  //computed: mapState(['equipe'])
+  // computed: {
+  //   e() {
+  //     return this.$store.state.equipe;
+  //   }
+  // }
+  // computed: mapState({
+  //   equipe: state => state.equipe
+  // })
+  computed: {
+    ...mapState({
+      enfermeiro: state => state.equipe.enfermeiro,
+      socorrista: state => state.equipe.socorrista,
+      medico: state => state.equipe.medico,
+      carro: state => state.equipe.carro,
+      telefone: state => state.equipe.telefone,
+      kitDeReanimacao: state => state.equipe.kitDeReanimacao,
+      tituloCustomizadoLocal(state) {
+        return `${this.titulo} - ${state.equipe.carro}`;
+      }
+    }),
+    corTitulo(){
+      let testeLogico = true;
+      if(testeLogico){
+        return 'text-danger';
+      }
+      return 'text-primary'
+    }
+  }
+}
+</script>
---
+++ emergenciasmedicas/src/components/ListaItens.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>        
+        <item 
+            v-for="(item, indice) in itens" 
+            :key="indice"
+            :dados="item"
+         />
+    </div>
+</template>
+
+<script>
+import Item from '@/components/Item.vue';
+import { mapState } from 'vuex';
+
+export default {
+    name: 'ListaItens',
+    components: { 
+        Item
+    },
+    props: {
+        tipo: String
+    },
+    computed: {
+        ...mapState({
+            enfermeiros: state => state.enfermeiros,
+            socorristas: state => state.socorristas,
+            medicos: state => state.medicos
+        }),
+        itens(){
+            switch(this.tipo){
+                case 'enfermeiros': return this.enfermeiros;
+                case 'socorristas': return this.socorristas;
+                case 'medicos': return this.medicos;
+            }
+
+            return [];
+
+        }
+    }
+}
+</script>
